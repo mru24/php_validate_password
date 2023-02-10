50b79f0bdd186f6844d18d1af4281efcdec12417
mru24 <mru24@yahoo.com>
2023-02-10 12:11:26 +0000

    jquery validation added

--- __index.php
+++ __index.php
@@ -12,6 +12,7 @@
       </form>
     </div>
   </body>
+  <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
 </html>
 <?php
 
